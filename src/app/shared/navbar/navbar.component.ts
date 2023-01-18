56c69a34866722e411830db8c8a6f48add30aeb6
Santiago Rivera <santiagoraika@gmail.com>
2023-01-18 08:42:29 -0500

    avances gráficas y validaciones en los nombres

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { AppState } from '../../app.reducer';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  
+  nombreUser: string = 'User Name'
+  userSub: Subscription;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.userSub = this.store.select('auth')
+    .pipe(
+      filter( ({user}) => user !== null)
+    )
+    .subscribe(({ user }) => this.nombreUser = user.nombre);
   }
 
 }
